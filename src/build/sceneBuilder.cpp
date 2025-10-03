5fe8247dd2163132f78e8d1bd164b94667017bec
Max Bebök <beboek.max@gmail.com>
2025-10-04 00:10:15 +0200

    wip: scene builder

--- src/build/sceneBuilder.cpp
+++ src/build/sceneBuilder.cpp
@@ -6,6 +6,8 @@
 #include "../utils/string.h"
 #include <filesystem>
 
+#include "../utils/binaryFile.h"
+
 namespace fs = std::filesystem;
 
 void Build::buildScene(Project::Project &project, const Project::SceneEntry &scene)
@@ -13,7 +15,16 @@ void Build::buildScene(Project::Project &project, const Project::SceneEntry &sce
   printf(" - Scene %d: %s\n", scene.id, scene.name.c_str());
   std::string fileName = "s" + Utils::padLeft(std::to_string(scene.id), '0', 4);
 
-  auto fsDataPath = fs::absolute(fs::path{project.getPath()} / "filesystem" / "p64");
-  auto sceneFile = fsDataPath / fileName;
+  std::unique_ptr<Project::Scene> sc{new Project::Scene(scene.id)};
 
+  auto fsDataPath = fs::absolute(fs::path{project.getPath()} / "filesystem" / "p64");
+
+  Utils::BinaryFile sceneFile{};
+  sceneFile.write<uint16_t>(sc->conf.fbWidth);
+  sceneFile.write<uint16_t>(sc->conf.fbHeight);
+  //sceneFile.write<uint32_t>(flags);
+  //sceneFile.writeColor(scene.clearColor);
+  //sceneFile.write<uint32_t>(scene.objects.length);
+
+  sceneFile.writeToFile(fsDataPath / fileName);
 }
